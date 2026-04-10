c50587b787e46a472d387039bc4075145b2c3518
Thuongdq <thuongdq@sapo.vn>
2026-04-10 09:35:41 +0700

    fixbug api

--- hay-hen/netlify/functions/api.js
+++ hay-hen/netlify/functions/api.js
@@ -3,8 +3,4 @@ import app from "../../server/index.js";
 
 export const handler = serverless(app, {
   basePath: ".netlify/functions/api",
-  request: {
-    method: "GET",
-    path: "/api/vietlott",
-  },
 });
